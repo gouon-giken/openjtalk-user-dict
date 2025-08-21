be4a3e5cb62af5dfc94cbf9ca1646d779fc312f6
WariHima <simamumin02@gmail.com>
2025-08-21 12:53:57 +0900

    remove low in build

---
+++ tool/remove_low.py
@@ -0,0 +1,31 @@
+from pathlib import Path
+
+from tqdm import tqdm
+
+
+def is_csv_file(file: Path) -> bool:
+    supported_extensions = [".csv"]
+    return file.suffix.lower() in supported_extensions
+
+def main():
+    input_dir_path = Path("./build")
+    csv_files = [file for file in input_dir_path.rglob("*") if is_csv_file(file)]
+
+    for file in csv_files:
+        data_list = []
+        text_list = file.read_text(encoding="utf-8").split("\n")
+
+        for line in tqdm(text_list):
+            split_line = line.split(",")
+
+            if len(split_line) == 15:
+                data_list.append(line)
+
+            else:
+                cur_line = ",".join( split_line[:15] )
+                data_list.append(cur_line)
+
+        file.write_text( "\n".join(data_list), encoding = "utf-8" )
+
+if __name__ == "__main__":
+    main()
