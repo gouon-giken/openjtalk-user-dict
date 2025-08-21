a8cf21b1f09b8ef37c48c80685b412edf5fa1d19
WariHima <simamumin02@gmail.com>
2025-08-21 18:09:26 +0900

    update

--- tool/remove_low.py
+++ tool/remove_low.py
@@ -1,13 +1,16 @@
 from pathlib import Path
 
 from tqdm import tqdm
-
+import re
 
 def is_csv_file(file: Path) -> bool:
     supported_extensions = [".csv"]
     return file.suffix.lower() in supported_extensions
 
 def main():
+
+    KANA_PATTERN = re.compile("[ァ-ヴー　]+")
+
     input_dir_path = Path("./build")
     csv_files = [file for file in input_dir_path.rglob("*") if is_csv_file(file)]
 
@@ -18,12 +21,24 @@ def main():
         for line in tqdm(text_list):
             split_line = line.split(",")
 
+
+            if "　" in split_line[12] and KANA_PATTERN.fullmatch(split_line[12]):
+                pron = split_line[12]
+                pron = pron.replace("　", "")
+                if pron == "":
+                    pron = "　"
+
+                print(pron)
+                new_split_line = split_line[:12] + [pron] + split_line[13:]
+                line = ",".join(new_split_line)
+
+
             if split_line[3] == "":
                 new_split_line = split_line[:3] + ["8000"] + split_line[4:]
                 split_line = new_split_line
 
                 line = ",".join(split_line)
-                
+
                 print(split_line[0])
 
 
@@ -36,7 +51,7 @@ def main():
             else:
                 cur_line = ",".join( split_line[:15] )
                 data_list.append(cur_line)
-
+        
         file.write_text( "\n".join(data_list), encoding = "utf-8" )
 
 if __name__ == "__main__":
